0c30bebaa2746156ebae92c6988febaee6418440
kubicekk <121161354+kubicekk@users.noreply.github.com>
2022-12-21 10:34:04 +0100

    Add files via upload

---
+++ banka.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ cout <<"Vita Vas Banka Helenka" <<endl;
+ cout <<"moznosti: \n\n";
+ cout <<"1) Prejete si darovat Vase penize bance? \n";
+ cout <<"2) Prejete si nam prozradit Vas PIN? \n";
+ cout <<"3) Prejete nam dat Vasi platebni kartu? \n";
+ cout <<"4) Prejete si aby se banka starala o Vas ucet? \n";
+ cout <<"5) Jestli nic nehcete nic tak nezdrzuj mym casem? \n";
+
+ int moznost;
+  cout << "Vyberte jednu moznost: \n";
+  cin >> moznost;
+
+switch (moznost)  {
+
+
+
+
+
+
+
+
+
+
+
+
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
